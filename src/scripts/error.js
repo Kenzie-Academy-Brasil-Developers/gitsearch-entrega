15670305c8be6021f9cf95795369201c397dbdcf
jonatasneres <jonatas102015@hotmail.com>
2025-01-07 17:55:52 -0300

    hotfix - change user and error on page error.index

--- src/scripts/error.js
+++ src/scripts/error.js
@@ -2,7 +2,7 @@ function returnToIndex() {
     const button = document.querySelector('button')
 
     button.addEventListener('click', ()=>{
-        window.location.replace('/gitsearch-entrega/')
+        window.location.replace('../../')
     })
 }
 
--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -71,7 +71,7 @@ async function changeUser() {
 
     if (button) {
         button.addEventListener('click', ()=>{
-            window.location.replace('/gitsearch-entrega/')
+            window.location.replace('../../')
             localStorage.clear()
         })
     }else{
